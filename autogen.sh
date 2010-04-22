717a023e2e833c3ea1a634b5c6f0f1807f5bf445
Stefan Westerfeld <stefan@space.twc.de>
2010-04-22 14:08:20 +0200

    Add basic automake infrastructure.

---
+++ autogen.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if automake --version >/dev/null 2>&1; then
+  :
+else
+  echo "You need to have automake installed to build this package"
+  DIE=1
+fi
+
+if autoconf --version >/dev/null 2>&1; then
+  :
+else
+  echo "You need to have autoconf installed to build this package"
+  DIE=1
+fi
+
+# bail out as scheduled
+test "0$DIE" -gt 0 && exit 1
+
+autoreconf -i && ./configure "$@"
